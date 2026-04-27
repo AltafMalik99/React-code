00f8ddbf92db4457fea2e56140a81481adad535c
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-28 00:33:43 +0500

    React components

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+export function NewFunction(){
+  
+  
+  
+  return (
+    <>
+    <h1 className="heading">new fuction</h1>
+    <button>button in new fuction</button>
+    <h1></h1>
+
+    </>
+  )
+}
+
+// export default  NewFunction;
+
+function App() {
+  return (
+    <div>
+
+    <h1>my app</h1>
+<NewFunction/>
+<NewFunction/>
+<NewFunction/>
+
+    </div>
+  )
+}
+
+export default App
+
+
+
+// video 24..
+
+
