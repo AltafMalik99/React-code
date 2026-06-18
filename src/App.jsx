fb6a1478e54a43ea753e948e075faeccd6e62835
Muhammad Altaf <zmaltaf99@gmail.com>
2026-06-19 00:23:11 +0500

    Dynamic Props Data

--- src/App.jsx
+++ src/App.jsx
@@ -240,53 +240,29 @@
 
 //  export default App
 
+// import React from "react"
+// import product from "./utils/product.js"
+//   <div className="p-4 bg-white border border-gray-200 hover:-translate-y-1 transition duration-300 rounded-lg shadow shadow-black/10 max-w-80">
+//             <img className="rounded-md max-h-40 w-full object-cover" src={products.image} alt="officeImage" />
+//             <p className="text-gray-900 text-xl font-semibold ml-2 mt-4">
+//                 {products.title}
+//             </p>
+//             <p className="text-zinc-400 text-sm/6 mt-2 ml-2 mb-2">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore..
+//             </p>
+//         </div>
+//     )
+//   })
+// }
 
 
+//     </div>
+//   )
+// }
+// modfy this card with simple react styling
 
 
+// export default App
 
-
-
-
-import React from 'react'
-import Button from './components/Button'
-import products from './utils/product'
-
-
-
-function App() {
-  return (
-    <div>
-  <Button title="this btn" subtitle="thia is second btn"/>
-    <Button title="this btn"/>
-
-
-{
-  products.map((products)=>{
-    return console.log("product",products);
-    
-  })
-}
-
-
-    </div>
-  )
-}
-
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// problem solve kar ne ha kal 
+// video  47
--- src/utils/product.js
+++ src/utils/product.js
@@ -1,21 +1,30 @@
-const products = [{
+const products = [
+    {
+    id:1,
     title:"shoes",
     price:"300",
-    descritpion:"this is braned shoes"
+    descritpion:"this is braned shoes",
+    image:"https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU="
 }
 ,
 
 {
+    id:2,
     title:"slipper",
     price:"300",
-    descritpion:"this is braned shoes"
+    descritpion:"this is braned shoes",
+    image:"https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU="
+
 }
 ,
 
 {
+    id:3,
     title:"sandals",
     price:"300",
-    descritpion:"this is braned shoes"
+    descritpion:"this is braned shoes",
+    image:"https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU="
+
 }] 
 
 export default products;
